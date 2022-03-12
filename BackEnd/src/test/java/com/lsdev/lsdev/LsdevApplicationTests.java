f3eaa4859170f9db88e07fa8eed5fb611153358c
lukeprog2021 <lsilva.2020@alunos.utfpr.edu.br>
2022-03-12 12:06:15 -0300

    Project created

---
+++ BackEnd/src/test/java/com/lsdev/lsdev/LsdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsdev.lsdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsdevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
