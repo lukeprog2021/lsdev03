4338a80121b4129b821a8fcbd5f03c0e5a8486ec
lukeprog2021 <lsilva.2020@alunos.utfpr.edu.br>
2022-03-12 19:35:29 -0300

    Erro

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
+      <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/devsuperior</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
     </div>
   );
 }
