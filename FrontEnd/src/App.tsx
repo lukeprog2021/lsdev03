bb5bb523b797b04cc64529b5d0383c6d871757c7
lukeprog2021 <lsilva.2020@alunos.utfpr.edu.br>
2022-03-13 11:47:47 -0300

    NavBar

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,22 +1,10 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import NavBar from "components/NavBar/NavBar";
 
 
 function App() {
   return (
     <div className="App">
-      <header>
-    <nav className="container">
-        <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
-                <div className="dsmovie-contact-container">
-                    <GithubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
-                </div>
-            </a>
-        </div>
-    </nav>
-</header>
+      <NavBar/>
     </div>
   );
 }
---
+++ FrontEnd/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>LSMovie</h1>
+          <a
+            href="https://github.com/lukeprog2021"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/LSDeveloper</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar
