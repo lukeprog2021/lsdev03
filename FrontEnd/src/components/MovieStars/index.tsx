d6b6ed7e2e7f1bf05ff9c02a9c80d941831a8f9b
lukeprog2021 <lsilva.2020@alunos.utfpr.edu.br>
2022-03-16 22:16:16 -0300

    Navigation Botons

--- FrontEnd/src/components/MovieStars/index.tsx
+++ FrontEnd/src/components/MovieStars/index.tsx
@@ -1,17 +1,18 @@
-import {ReactComponent as StarFull} from "assets/img/starfull.svg"
-import {ReactComponent as StarEmpty} from "assets/img/star-empty.svg"
-import {ReactComponent as StarHalf} from "assets/img/star-half.svg"
-import "./style.css"
+import { ReactComponent as StarFull } from "assets/img/starfull.svg";
+import { ReactComponent as StarEmpty } from "assets/img/star-empty.svg";
+import { ReactComponent as StarHalf } from "assets/img/star-half.svg";
+import "./style.css";
 
-
-function MovieStars(){
-    return(<div className="dsmovie-stars-container">
-    <StarFull />
-    <StarFull />
-    <StarFull />
-    <StarHalf />
-    <StarEmpty />
-  </div>)
+function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
 }
 
-export default MovieStars
+export default MovieStars;
